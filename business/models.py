1c39d79492208e86489152be0dd5c793256e985b
orgjr <orgj@protonmail.com>
2026-06-25 15:07:58 -0300

    refactor: replace Store with Business entity

---
+++ business/models.py
@@ -0,0 +1,27 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from user.models import UserModel
+
+
+# Create your models here.
+class Business(models.Model):
+    cnpj = models.CharField(max_length=14, unique=True)
+    razao_social = models.CharField(max_length=100, unique=True)
+    nome_fantasia = models.CharField(max_length=256)
+    user_model = models.OneToOneField(
+        UserModel, related_name="business", on_delete=models.CASCADE, default="business"
+    )
+
+    def __str__(self):
+        return self.razao_social
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if self.user_model.client_type != "business":
+            raise ValidationError(
+                {"client_type": 'Business must be client_type="business"'}
+            )
