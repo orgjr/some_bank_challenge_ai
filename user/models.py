2920830d25da60723870da0538f98a1fa2f948a4
orgjr <orgj@protonmail.com>
2026-06-25 14:54:57 -0300

    feat: enforce user role consistency and superuser support

--- user/models.py
+++ user/models.py
@@ -1,24 +1,53 @@
-from django.core.exceptions import ValidationError
-from django.db import models
+import uuid
 
-from client.models import ClientModel
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+from django.db.models import Q
+
+from user.manager import UserManager
 
 
 # Create your models here.
-class UserModel(models.Model):
-    cpf = models.CharField(max_length=11, unique=True)
-    name = models.CharField(max_length=128)
-    client = models.OneToOneField(
-        ClientModel, related_name="user", on_delete=models.CASCADE
+class UserModel(AbstractBaseUser):
+    uuid = models.UUIDField(
+        default=uuid.uuid4, unique=True, editable=False, primary_key=True
     )
+    email = models.EmailField(unique=True)
+
+    class ClientType(models.TextChoices):
+        BUSINESS = "business", "business"
+        PERSON = "person", "person"
+
+    client_type = models.CharField(
+        max_length=8, choices=ClientType, null=True, blank=True
+    )
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=(Q(is_superuser=True) & Q(client_type__isnull=True))
+                | (Q(is_superuser=False) & Q(client_type__isnull=False)),
+                name="superuser_or_client",
+            ),
+        ]
 
     def __str__(self):
-        return self.name
+        return self.email
 
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
 
-    def clean(self):
-        if self.client.client_type != "user":
-            raise ValidationError("Client must be 'user' type.")
+    def get_client_name(self):
+        if hasattr(self, "person"):
+            return self.person.name
+        if hasattr(self, "business"):
+            return self.business.razao_social
+
+        return None
