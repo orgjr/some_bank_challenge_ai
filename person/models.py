f9dea55aee8ef1c9086a827f0f3d12acd404061c
orgjr <orgj@protonmail.com>
2026-06-25 15:05:10 -0300

    refactor: introduce person profile for individual clients

---
+++ person/models.py
@@ -0,0 +1,26 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from user.models import UserModel
+
+
+# Create your models here.
+class Person(models.Model):
+    cpf = models.CharField(max_length=11, unique=True)
+    name = models.CharField(max_length=128)
+    user_model = models.OneToOneField(
+        UserModel, related_name="person", on_delete=models.CASCADE, default="user"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if self.user_model.client_type != "person":
+            raise ValidationError(
+                {"client_type": 'Person must be client_type="person" type'}
+            )
