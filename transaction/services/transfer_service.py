faa71787dccd077f5ca1ea82ae28e5d39ec3c346
orgjr <orgj@protonmail.com>
2026-06-25 16:15:04 -0300

    refactor: rename and update TransferService to match model changes

--- transaction/services/transfer_service.py
+++ transaction/services/transfer_service.py
@@ -6,30 +6,26 @@ from django.db.transaction import atomic
 from requests.exceptions import RequestException
 from rest_framework.exceptions import ValidationError
 
-from account.models import AccountModel
-from transaction.models import TransactionModel
+from account.models import Account
+from transaction.models import Transaction
 from transaction.services.authorization_service import AuthorizationService
 from transaction.services.rollback_service import RollbackService
 
 logger = logging.getLogger(__name__)
 
 
-class TransactionService:
+class TransferService:
     ### challenge business rule
     @staticmethod
     def execute_transfer(validated_data):
         transfer_completed = False
         try:
             AuthorizationService.authorization_service_request()
-            transaction = TransactionModel(**validated_data)
+            transaction = Transaction(**validated_data)
 
             with atomic():
-                payer = AccountModel.objects.select_for_update().get(
-                    pk=transaction.payer.pk
-                )
-                payee = AccountModel.objects.select_for_update().get(
-                    pk=transaction.payee.pk
-                )
+                payer = Account.objects.select_for_update().get(pk=transaction.payer.pk)
+                payee = Account.objects.select_for_update().get(pk=transaction.payee.pk)
                 transaction.allow_transfer()
 
                 # payer
@@ -48,5 +44,5 @@ class TransactionService:
                 RollbackService.rollback_due_to_inconsistency(validated_data)
             raise ValidationError(e)
         except RequestException as e:
-            logger.exception("Authorization service rejected transaction")
-            raise ValidationError("Unauthorized transaction") from e
+            logger.exception("authorization service rejected transaction")
+            raise ValidationError("unauthorized transaction") from e
