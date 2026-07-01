58ae53ddf5d91e394e7190c69f9a5f6132b58c71
orgjr <orgj@protonmail.com>
2026-07-01 19:43:23 -0300

    feat: add Django REST Framework pagination to list endpoints

--- bank_challenge_ai/settings/base.py
+++ bank_challenge_ai/settings/base.py
@@ -77,6 +77,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
