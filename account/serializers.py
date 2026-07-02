bd9ee0b567672504fafd200a5a7862e63d1e6ce8
orgjr <orgj@protonmail.com>
2026-07-01 21:16:33 -0300

    chore: remove development defaults from API schema fields

--- account/serializers.py
+++ account/serializers.py
@@ -5,9 +5,9 @@ from account.models import Account
 
 class AccountResponseSerializer(serializers.Serializer):
     client = serializers.CharField(source="client.get_name")
-    email = serializers.CharField(source="client.email", default="user@example.com")
-    agency = serializers.CharField(default="1234")
-    number = serializers.CharField(default="1234567")
+    email = serializers.CharField(source="client.email")
+    agency = serializers.CharField()
+    number = serializers.CharField()
 
     class Meta:
         model = Account
