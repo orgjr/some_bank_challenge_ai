2fee07a35b0fff7c13afb697109c97b8810ea0d5
orgjr <orgj@protonmail.com>
2026-07-01 21:11:00 -0300

    feat: enhance transaction response schema for API documentation

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,19 +1,26 @@
+import logging
+from decimal import Decimal
+
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from account.models import Account
+from transaction.models import Transaction
+
+logger = logging.getLogger(__name__)
 
 
-class TransferSerializer(serializers.Serializer):
+class CreateTransferSerializer(serializers.Serializer):
     value = serializers.DecimalField(
         max_digits=8,
         decimal_places=2,
         error_messages={
             "invalid": "value must be numeric with a maximum of two places after the decimal point"
         },
+        default=Decimal("0.00"),
     )
     payee = serializers.IntegerField(
-        error_messages={"invalid": "payee must be a valid account number"}
+        error_messages={"invalid": "payee must be a valid account number"},
     )
 
     def validate_payee(self, value):
@@ -22,3 +29,14 @@ class TransferSerializer(serializers.Serializer):
             return value
         except Account.DoesNotExist:
             raise ValidationError({"payee": "payee account not found"})
+
+
+class ResponseTransferSerializer(serializers.Serializer):
+    value = serializers.DecimalField(max_digits=8, decimal_places=2)
+    payer = serializers.CharField(source="payer.client")
+    payee = serializers.CharField(source="payee.client")
+    transaction_type = serializers.CharField(source="get_transaction_type_display")
+    operation_date = serializers.DateTimeField()
+
+    class Meta:
+        model = Transaction
